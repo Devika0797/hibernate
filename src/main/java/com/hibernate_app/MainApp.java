3873c8fe2e9d302c40532c83e9d019ec2edb5f51
Devika0797 <devika-97@mail.ru>
2021-12-29 13:38:40 +0300

    Initial commit

---
+++ src/main/java/com/hibernate_app/MainApp.java
@@ -0,0 +1,24 @@
+package com.hibernate_app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+public class MainApp {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure()
+                .addAnnotatedClass(Product.class).
+                        buildSessionFactory();// "hibernate.cfg.xml"
+
+        //create
+        Session session = null;
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        Product product = new Product("Apple",70);
+        System.out.println(product);
+        session.save(product);
+        System.out.println(product);
+        session.getTransaction().commit();
+
+        sessionFactory.close();
+    }
+}
